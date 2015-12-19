6b44a3f0a3da0bd49448cfefee0e03c1b5931c36
Matthew Campbell <whariki@gmail.com>
2015-12-20 07:57:28 +1300

    Set framework up for configuration tests

---
+++ test/test_config.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+
+context('Test Suite: config.js unit tests', function {
+
+describe('Command Line Args', function {
+    it('should start with no errors if no command line args', function () {
+
+    })
+});
+
+
+});
