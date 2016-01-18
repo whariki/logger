e090f6426b33fbf66db209a1807ec948907b19a4
Matthew Campbell <whariki@gmail.com>
2016-01-18 21:02:40 +1300

    test config working added middleware and started experimenting with routes

--- logger.js
+++ logger.js
@@ -1,14 +1,20 @@
 var myConfig = require('./config.js');
 var express = require('express');
+var middleware = require('./app/middleware');
+var routes = require('./app/routes');
 var logger = express();
 
 var default_port = myConfig.port();
 var default_interface = myConfig.hostname();
 
+
+
 // set up routers and middleware
-logger.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+middleware(logger);
+routes(logger);
+//logger.get('/', function (req, res) {
+//    res.send('Hello World!');
+//});
 
 var server = logger.listen(default_port, default_interface, function () {
     var host = server.address().address;
--- test/test_config.js
+++ test/test_config.js
@@ -17,7 +17,8 @@ context('Test Suite: config.js unit tests: ', function() {
 describe('Command Line Args Only', function() {
     var config_file_exists = true;
 
-    this.slow(20000);
+    this.slow(2000);
+    this.timeout(20000);
 
     before(function() {
         // renames the logger.config to remove it from the test.
@@ -84,7 +85,7 @@ describe('Command Line Args Only', function() {
             });
 
             uut.stdout.on('data', function (data) {
-                //console.log('got data: %s',data);
+                console.log('got data: %s',data);
                 assert.equal(`Logger app listening at http://${default_listen_host}:${default_listen_port}\n`,decoder.write(data));
                 done();
                 uut_running = false;
@@ -115,10 +116,9 @@ describe('Command Line Args Only', function() {
         });
     });
 
-    var hostnameUnderTest = '127.0.1.1';
 
-    describe(`command line specified hostname as an ip address of ${hostnameUnderTest}`, function() {
-        //var hostnameUnderTest = 'X'; // '127.0.1.1'
+    describe(`command line specified hostname as an ip address`, function() {
+        var hostnameUnderTest = '127.0.1.1';
         var uut = spawn('node', ['logger.js', '-h', `${hostnameUnderTest}`], {encoding:'utf8'});
 
         var uut_running = true;
@@ -129,7 +129,7 @@ describe('Command Line Args Only', function() {
 
         //console.log("logger process running as pid %s",uut.pid)
 
-        it('start logger.js', function (done) {
+        it(`start logger.js -h ${hostnameUnderTest}`, function (done) {
             var myTimeout = setTimeout(function() {
                 uut.kill();
                 uut_running = false;
@@ -143,8 +143,121 @@ describe('Command Line Args Only', function() {
             });
 
             uut.stdout.on('data', function (data) {
-                //console.log('got data: %s',data);
+                console.log('got data: %s',data);
                 assert.equal(`Logger app listening at http://${hostnameUnderTest}:${default_listen_port}\n`,decoder.write(data));
+                done();
+                uut_running = false;
+                uut.kill();
+            });
+
+            uut.stderr.on('data', function (data) {
+                //console.log('error data: %s',data);
+                test_error_reported = true;
+                done();
+                uut.kill();
+            });
+            uut.stdin.end();
+        });
+
+        // wait for process to spawn and return
+        it('should start with no errors', function () {
+            for(var count = 0; (uut_running  && count < 10000/100); count++) {
+                sleep.usleep(100);
+                count++;
+            };
+            assert.equal(false,test_timed_out);
+            assert.equal(false,test_error_reported);
+            uut.kill();
+        });
+    });
+
+
+
+    describe(`command line specified port`, function() {
+        var portUnderTest = '6555';
+        var uut = spawn('node', ['logger.js', '-p', `${portUnderTest}`], {encoding:'utf8'});
+
+        var uut_running = true;
+        var test_timed_out = false;
+        var test_error_reported = false;
+
+        //init_logger_process(uut, uut_running, test_timed_out);
+
+        //console.log("logger process running as pid %s",uut.pid)
+
+        it(`start logger.js -p ${portUnderTest}`, function (done) {
+            var myTimeout = setTimeout(function() {
+                uut.kill();
+                uut_running = false;
+                //console.log("pid %s timed out",uut.pid)
+                test_timed_out = true;
+            }, 10000);
+
+            uut.on('exit', function(code, signal) {
+                //console.log('exit event');
+                uut_running = false;
+            });
+
+            uut.stdout.on('data', function (data) {
+                console.log('got data: %s',data);
+                assert.equal(`Logger app listening at http://${default_listen_host}:${portUnderTest}\n`,decoder.write(data));
+                done();
+                uut_running = false;
+                uut.kill();
+            });
+
+            uut.stderr.on('data', function (data) {
+                //console.log('error data: %s',data);
+                test_error_reported = true;
+                done();
+                uut.kill();
+            });
+            uut.stdin.end();
+        });
+
+        // wait for process to spawn and return
+        it('should start with no errors', function () {
+            for(var count = 0; (uut_running  && count < 10000/100); count++) {
+                sleep.usleep(100);
+                count++;
+            };
+            assert.equal(false,test_timed_out);
+            assert.equal(false,test_error_reported);
+            uut.kill();
+        });
+    });
+
+
+
+    describe(`command line specified port and host`, function() {
+        var portUnderTest = '55555';
+        var hostnameUnderTest = '127.1.0.1';
+        var uut = spawn('node', ['logger.js', '-p', `${portUnderTest}`, '-h', `${hostnameUnderTest}`], {encoding:'utf8'});
+
+        var uut_running = true;
+        var test_timed_out = false;
+        var test_error_reported = false;
+
+        //init_logger_process(uut, uut_running, test_timed_out);
+
+        //console.log("logger process running as pid %s",uut.pid)
+
+        it(`start logger.js -p ${portUnderTest} -h ${hostnameUnderTest}`, function (done) {
+            var myTimeout = setTimeout(function() {
+                uut.kill();
+                uut_running = false;
+                //console.log("pid %s timed out",uut.pid)
+                test_timed_out = true;
+            }, 10000);
+
+            uut.on('exit', function(code, signal) {
+                //console.log('exit event');
+                uut_running = false;
+            });
+
+            uut.stdout.on('data', function (data) {
+                console.log('got data: %s',data);
+                assert.equal(`Logger app listening at http://${hostnameUnderTest}:${portUnderTest}\n`,decoder.write(data));
                 done();
                 uut_running = false;
                 uut.kill();
